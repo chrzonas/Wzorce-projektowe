f716bd57afa4a86d50ae8676a4d8947b02f8458b
chrzonas <chrzonas@o2.pl>
2018-10-14 16:52:16 +0200

    Bridge 3.1.1

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,43 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+	
+	private static final int empty_stack = -1;
+	private static final int full_stack = 11;
+	private static final int stack_capacity = 12;
+
+	private int[] NUMBERS = new int[stack_capacity];
+
+	private int total = empty_stack;
+
+	protected int getTotal()
+	{
+		return total;
+	}
+
+	protected void countIn(final int in) {
+		if (!isFull())
+			NUMBERS[++total] = in;
+	}
+
+	protected boolean callCheck() {
+		return total == empty_stack;
+	}
+
+	protected boolean isFull() {
+		return total == full_stack;
+	}
+
+	protected int peekaboo() {
+		if (callCheck())
+			return empty_stack;
+		return NUMBERS[total];
+	}
+
+	protected int countOut() {
+		if (callCheck())
+			return empty_stack;
+		return NUMBERS[total--];
+	}
+
+}
